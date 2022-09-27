7183e3cd542cec3c26cbb00f0af3425ce70ed25e
Ashpan Raskar <ashpanr@gmail.com>
2022-09-26 21:04:33 -0400

    completed part b

--- Lab1/part_b.m
+++ Lab1/part_b.m
@@ -8,12 +8,32 @@ R_matrix = A(:,:,1); G_matrix = A(:,:,2); B_matrix = A(:,:,3);
 [rows,cols,depth] = size(A);
 RAND_matrix = zeros(rows,cols,depth);
 A_encrypted = zeros(rows,cols,depth);
+c = 1;
 for i = 1:rows
     for j = 1:cols
         for k = 1:depth
-            RAND_matrix(i, j, k) = rand_nums(i+j+k);
-            
+            if c == (width(rand_nums))
+                c = 1;
+            else
+                c = c + 1;
+            end
+            RAND_matrix(i, j, k) = rand_nums(c);
+            A_encrypted(i, j, k) = uint8(bitxor(A(i,j,k), RAND_matrix(i,j,k)));
         end
     end
 end
-
+A_decrypted = zeros(rows,cols,depth);
+c = 1;
+for i = 1:rows
+    for j = 1:cols
+        for k = 1:depth
+            if c == (width(rand_nums))
+                c = 1;
+            else
+                c = c + 1;
+            end
+            RAND_matrix(i, j, k) = rand_nums(c);
+            A_decrypted(i, j, k) = uint8(bitxor(A_encrypted(i,j,k), RAND_matrix(i,j,k)));
+        end
+    end
+end
